9b590065f4b2ec00dd122221eaa5cf63bcf0dc6e
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2022-01-28 13:50:37 -0300

    finished style

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,106 +9,108 @@ import './index.scss'
 export function Home() {
   return (
     <>
+
       <body>
-      <header>
-        <Header />
-      </header>
-      <form action="">
-      <div className="div-search">
-        <div className="components-search">
-          <div className="search-id search-components">
-            <p>ID</p>
-            <input type="number" />
+        <header>
+          <Header />
+        </header>
+        <main>
+          <form action="">
+            <div className="div-search">
+              <div className="components-search">
+                <div className="search-id search-components">
+                  <p>ID</p>
+                  <input type="number" />
+                </div>
+
+                <div className="search-symbol search-components">
+                  <p>Símbolo</p>
+                  <input type="text" />
+                </div>
+
+                <div className="search-name search-components">
+                  <p>Nome</p>
+                  <input type="text" />
+                </div>
+
+                <div className="search-date-register search-components">
+                  <p>Data de Cadastro</p>
+                  <input type="date" />
+                </div>
+
+                <div className="search-date-alteration search-components">
+                  <p>Data de Alteração</p>
+                  <input type="date" />
+                </div>
+
+                <button className="search search-buttons">Pesquisar</button>
+
+              </div>
+            </div>
+          </form>
+          <div className="div-include">
+            <a href="#" className="button-include">+ Incluir Registro</a>
           </div>
 
-          <div className="search-symbol search-components">
-            <p>Símbolo</p>
-            <input type="text" />
-          </div>
+          <table id="data-table">
+            <thead>
+              <tr className="table-description">
+                <th>ID</th>
+                <th>Símbolo</th>
+                <th>Nome</th>
+                <th>Data de Cadastro</th>
+                <th>Data de Alteração</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
 
-          <div className="search-name search-components">
-            <p>Nome</p>
-            <input type="text" />
-          </div>
+            <tbody>
+              <tr>
+                <td className="id">133</td>
+                <td className="symbol">ATT15</td>
+                <td className="name">American top Team</td>
+                <td className="dateInclude">27/01/2022</td>
+                <td className="dateAlteration">27/01/2022</td>
+                <td className="edit"><img src={editing} alt="Editar" /> </td>
+                <td className="del"> <img src={del} alt="Excluir" /> </td>
+              </tr>
 
-          <div className="search-date-register search-components">
-            <p>Data de Cadastro</p>
-            <input type="date" />
-          </div>
+              <tr>
+                <td className="id">133</td>
+                <td className="symbol">ATT15</td>
+                <td className="name">American top Team</td>
+                <td className="dateInclude">27/01/2022</td>
+                <td className="dateAlteration">27/01/2022</td>
+                <td className="edit"><img src={editing} alt="Editar" /> </td>
+                <td className="del"> <img src={del} alt="Excluir" /> </td>
+              </tr>
 
-          <div className="search-date-alteration search-components">
-            <p>Data de Alteração</p>
-            <input type="date" />
-          </div>
+              <tr>
+                <td className="id">133</td>
+                <td className="symbol">ATT15</td>
+                <td className="name">American top Team</td>
+                <td className="dateInclude">27/01/2022</td>
+                <td className="dateAlteration">27/01/2022</td>
+                <td className="edit"><img src={editing} alt="Editar" /> </td>
+                <td className="del"> <img src={del} alt="Excluir" /> </td>
+              </tr>
 
-          <button className="search search-buttons">Pesquisar</button>
-
-        </div>
-      </div>
-      </form>
-
-      <div className="div-include">
-      <a href="#" className="button-include">+ Incluir Registro</a>
-      </div>
-
-      <table id="data-table">
-        <thead>
-          <tr className="table-description">
-            <th>ID</th>
-            <th>Símbolo</th>
-            <th>Nome</th>
-            <th>Data de Cadastro</th>
-            <th>Data de Alteração</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-
-        <tbody>
-          <tr>
-            <td className="id">133</td>
-            <td className="symbol">ATT15</td>
-            <td className="name">American top Team</td>
-            <td className="dateInclude">27/01/2022</td>
-            <td className="dateAlteration">27/01/2022</td>
-            <td className="edit"><img src={editing} alt="Editar" /> </td>
-            <td className="del"> <img src={del} alt="Excluir" /> </td>
-          </tr>
-
-          <tr>
-            <td className="id">133</td>
-            <td className="symbol">ATT15</td>
-            <td className="name">American top Team</td>
-            <td className="dateInclude">27/01/2022</td>
-            <td className="dateAlteration">27/01/2022</td>
-            <td className="edit"><img src={editing} alt="Editar" /> </td>
-            <td className="del"> <img src={del} alt="Excluir" /> </td>
-          </tr>
-
-          <tr>
-            <td className="id">133</td>
-            <td className="symbol">ATT15</td>
-            <td className="name">American top Team</td>
-            <td className="dateInclude">27/01/2022</td>
-            <td className="dateAlteration">27/01/2022</td>
-            <td className="edit"><img src={editing} alt="Editar" /> </td>
-            <td className="del"> <img src={del} alt="Excluir" /> </td>
-          </tr>
-
-          <tr>
-            <td className="id">133</td>
-            <td className="symbol">ATT15</td>
-            <td className="name">American top Team</td>
-            <td className="dateInclude">27/01/2022</td>
-            <td className="dateAlteration">27/01/2022</td>
-            <td className="edit"><img src={editing} alt="Editar" /> </td>
-            <td className="del"> <img src={del} alt="Excluir" /> </td>
-          </tr>
-        </tbody>
-      </table>
+              <tr>
+                <td className="id">133</td>
+                <td className="symbol">ATT15</td>
+                <td className="name">American top Team</td>
+                <td className="dateInclude">27/01/2022</td>
+                <td className="dateAlteration">27/01/2022</td>
+                <td className="edit"><img src={editing} alt="Editar" /> </td>
+                <td className="del"> <img src={del} alt="Excluir" /> </td>
+              </tr>
+            </tbody>
+          </table>
+        </main>
+        <Footer />
       </body>
 
-      <Footer/>
 
     </>
   )
